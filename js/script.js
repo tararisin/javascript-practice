a0c18a3baf1815d94931be6d43a166543b190019
tara noble <42617190+tararisin@users.noreply.github.com>
2021-05-17 19:43:49 -0700

    added single & multilinecomment + console name & fav food

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//single line comment//
+/*multi line comment multi line comment multi line comment 
+multi line commentmulti line comment*/
+
+console.log('tara noble/pizza');
+
+
+            
+
+
+
